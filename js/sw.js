5c24dcb6f669247caf174bf75ed981ba45f7d774
abisuresh <aesther2012@gmail.com>
2018-08-31 13:38:23 -0400

    Making images responsive

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,9 @@
 //Based on tutorial in https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
 // const request = new request();
+if('caches' in window){
+
+}
+
 self.addEventListener(){
   event.waitUntil(
     caches.open(cacheName).then(function(cache){
