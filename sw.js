2019ae80ef05e28fc28f1fd676827f1be549fe97
abisuresh <aesther2012@gmail.com>
2018-09-03 14:09:17 -0400

    Continuing to work on service worker and styling

--- sw.js
+++ sw.js
@@ -1,25 +1,31 @@
 //Based on tutorial in https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+//https://developers.google.com/web/fundamentals/primers/service-workers/
 // const request = new request();
 
 var cacheName = "Restaurants";
+var pagesToCache = [
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/index.html',
+  '/restaurant.html'
+]
+
+//Installing service worker
 self.addEventListener('install',function(){
   event.waitUntil(
     caches.open(cacheName).then(function(cache){
-      return cache.addAll(
-        [
-          '/css/styles.css',
-          '/js/dbhelper.js',
-          '/js/main.js',
-          '/js/restaurant_info.js',
-          '/data/restaurants.json',
-          '/index.html',
-          '/restaurant.html'
-        ]
-      );
+      return cache.addAll(pagesToCache)
     })
-    );
-  });
+  );
+});
 
-  if('caches' in window){
-    caches.open(cacheName);
-  }
+//Opening cached files
+
+if('caches' in window){
+  caches.open(cacheName);
+}
+
+//Updating service worker
